dd89f52fc4991177fecb5f3465fd081eb11708a8
lesliechiri <dianawantes@gmail.com>
2019-05-02 19:58:24 +0300

    adding dir

--- shapes.py
+++ shapes.py
@@ -1,18 +1,20 @@
 class Circle:
 
 
-    def __init__(self):
-        self.r = 0
+    def __init__(self,r):
+        self.r = r
 
 
 
-    def area(self,r):
+    def area(self):
+        r = self.r
         
         A = 3.14*r*r
 
         return A
 
-    def circumference(self,r):
+    def circumference(self):
+        r = self.r
         C = 2*3.14*r
 
         return C
@@ -21,44 +23,52 @@ class Circle:
 
 class Square:
     def __init__(self):
-        self.a = 0
+        self.a = a
 
-    def area(self,a):
+    def area(self):
+        a = self.a
         A = a*a
 
         return A
 
     def perimeter(self):
+        a = self.a
         P = 4*a
 
         return P
 
 class Rectangle:
-    def __init__(self):
-        self.w = 0
-        self.l = 0
+    def __init__(self,w,l):
+        self.w = w
+        self.l = l
 
-    def area(self,w,l):
+    def area(self):
+        w = self.w
+        l = self.l
         A = w*l
 
         return A
 
     def perimeter(self):
+        w = self.w
+        l = self.l
         P = 2*(w+l)
 
         return P
 
 
 class Sphere:
-    def __init__(self):
-        self.r = 0
+    def __init__(self,r):
+        self.r = r
 
-    def surface_area(self,r):
+    def surface_area(self):
+        r = self.r
         SA = 4*3.14*r*r
 
         return SA
 
-    def volume(self,r):
+    def volume(self):
+        r = self.r
         V = 4/3*3.14*r*r*r  
 
         return V                                      
